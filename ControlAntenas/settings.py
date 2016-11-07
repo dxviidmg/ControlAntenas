6b68194d2c773e95a87df451526bf23d5b7e049c
David <david@david-Lenovo-U310>
2016-11-07 13:53:26 -0600

    configurando archivos estaticos

--- ControlAntenas/settings.py
+++ ControlAntenas/settings.py
@@ -121,13 +121,20 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = 'staticfiles'
-STATICFILES_DIRS = (os.path.join(BASE_DIR,'static'),)
+#STATIC_URL = '/static/'
+#STATIC_ROOT = 'staticfiles'
+#STATICFILES_DIRS = (os.path.join(BASE_DIR,'static'),)
 
-MEDIA_ROOT = os.path.join(BASE_DIR,'media')
-MEDIA_URL = '/media/'
+#MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+#MEDIA_URL = '/media/'
 
 from django.core.urlresolvers import reverse_lazy
 
 LOGOUT_REDIRECT_URL = reverse_lazy('clientes:login')
+
+STATIC_URL = '/static/'
+STATIC_ROOT = 'static'
+STATICFILES_DIRS = (os.path.join(BASE_DIR,'staticfiles'),)
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
